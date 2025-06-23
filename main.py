c57d4195c8c00f9072a46f430ac2ac631ef7c04b
hiimchinh <chinhhm@proton.me>
2025-06-23 23:14:29 +0700

    add verbose param

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ client = genai.Client(api_key=api_key)
 
 
 user_prompt = sys.argv[1]
+argv = sys.argv 
+
+is_verbose = '--verbose' in argv
+
 if not user_prompt:
     print("Usage: python main.py <prompt>")
     sys.exit(1)
@@ -19,6 +23,7 @@ messages = [
 ]
 res = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-
-print("Prompt tokens: " + str(res.usage_metadata.prompt_token_count))
-print("Response tokens: " + str(res.usage_metadata.candidates_token_count))
+if is_verbose:
+    print(f"User prompt: {user_prompt}")
+    print("Prompt tokens: " + str(res.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(res.usage_metadata.candidates_token_count))
